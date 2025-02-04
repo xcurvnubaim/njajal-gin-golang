3b203194fd0bb43a52ea3c0e68a1e63b289a7b54
xcurvnubaim <zhariffmarzuqi@gmail.com>
2025-02-04 23:51:57 +0700

    feat: add pagination response

--- internal/pkg/app/response.go
+++ internal/pkg/app/response.go
@@ -1,5 +1,7 @@
 package app
 
+import "github.com/xcurvnubaim/njajal-gin-golang/internal/pkg/query"
+
 type SuccessResponse[D any] struct {
 	Status  bool    `json:"status"`
 	Message string  `json:"message"`
@@ -12,6 +14,13 @@ type ErrorResponse struct {
 	Message string  `json:"message"`
 	Data    *string `json:"data"`
 	Error   *string `json:"error"`
+}
+
+type PaginationResponse[D any] struct {
+	Status  bool                  `json:"status"`
+	Message string                `json:"message"`
+	Meta    *query.PaginationMeta `json:"meta"`
+	Data    *D                    `json:"data"`
 }
 
 func NewSuccessResponse[D any](message string, data *D) *SuccessResponse[D] {
@@ -29,5 +38,14 @@ func NewErrorResponse(message string, err *string) *ErrorResponse {
 		Message: message,
 		Data:    nil,
 		Error:   err,
+	}
+}
+
+func NewPaginationResponse[D any](message string, meta *query.PaginationMeta, data *D) *PaginationResponse[D] {
+	return &PaginationResponse[D]{
+		Status:  true,
+		Message: message,
+		Meta:    meta,
+		Data:    data,
 	}
 }
